b2c4248ca7937cb89a3104768d5779c092b1b1cf
Guy Needham <guy.l.needham@gmail.com>
2015-02-09 19:41:23 +0000

    Update LargestPrimeFactor.scala

--- com/gn/euler/LargestPrimeFactor.scala
+++ com/gn/euler/LargestPrimeFactor.scala
@@ -6,10 +6,17 @@ class LargestPrimeFactor {
 	def largest(n: Long): Long = {
 		// firstly find a stream of all prime Ints
 		lazy val ps: Stream[Int] =
+		        // stream starting at 2
+		        // going on to infinity
+		        // built with recursive call to ps
 			2 #:: ps.map(i =>
+			                // map value to a stream of values
 					Stream.from(i + 1).
+					    // find the next value satisfying this 
 					    find(j =>
+					           // all values where k^2 is less than the value j and j is prime 
 					           ps.takeWhile(k => k * k <= j).forall(j % _ > 0)
+					           // get method returns value from an Option type
 				                 ).get
 				     ) 
   
